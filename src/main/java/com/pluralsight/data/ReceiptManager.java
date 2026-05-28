c41f03313126c7361c15356a4dddf25a4a9b25ce
Michael Okeke <michaelokeke123@gmail.com>
2026-05-28 11:29:09 -0500

    Added: created receipt manager class to save order.

--- src/main/java/com/pluralsight/data/ReceiptManager.java
+++ src/main/java/com/pluralsight/data/ReceiptManager.java
@@ -1,4 +1,44 @@
 package com.pluralsight.data;
 
+import com.pluralsight.models.MenuItem;
+import com.pluralsight.models.Order;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.stream.Collectors;
+
 public class ReceiptManager {
+    public static void saveReceipt(Order order) {
+
+        try {
+
+            String timestamp = LocalDateTime.now()
+                    .format(DateTimeFormatter.ofPattern("yyyyMMdd-HHmmss"));
+
+            String fileName = "receipts/" + timestamp + ".txt";
+
+            String receiptText = order.getItems().stream()
+                    .map(MenuItem::toString)
+                    .collect(Collectors.joining("\n"));
+
+            receiptText += String.format(
+                    "%n%nTOTAL: $%.2f",
+                    order.getTotal()
+            );
+
+            Files.write(
+                    Path.of(fileName),
+                    receiptText.getBytes()
+            );
+
+            System.out.println("Receipt saved!");
+
+        } catch (IOException e) {
+
+            System.out.println("Error saving receipt.");
+        }
+    }
 }
